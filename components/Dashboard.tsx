871d9fad960310a51b967afd6c2717ddc59cb155
Mateja <markojovanovicc23@outlook.com>
2026-04-18 09:43:39 +0200

    added skeleton to dashboard while loading user

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,27 +3,41 @@
 import { useUser } from "@clerk/nextjs";
 import { Clock, Star, UserCog } from "lucide-react";
 import { ReactNode } from "react";
+import DashboardSkeleton from "./DashboardSkeleton";
 
 type CardColor = "blue" | "green" | "red";
 
-interface Card { 
+interface Card {
   id: number;
   name: string;
   icon: ReactNode;
   color: CardColor;
-};
+}
 
 const cards: Card[] = [
-  { id: 0, name: "Registrovani predavači", icon: <UserCog size={20} />, color: "blue" },
+  {
+    id: 0,
+    name: "Registrovani predavači",
+    icon: <UserCog size={20} />,
+    color: "blue",
+  },
   { id: 1, name: "Recenzije", icon: <Star size={20} />, color: "green" },
-  { id: 2, name: "Poslednji put viđen/a", icon: <Clock size={20} />, color: "red" },
+  {
+    id: 2,
+    name: "Poslednji put viđen/a",
+    icon: <Clock size={20} />,
+    color: "red",
+  },
 ];
 
-const colorMap: Record<CardColor, {
-  bar: string;
-  iconBg: string;
-  border: string;
-}> = {
+const colorMap: Record<
+  CardColor,
+  {
+    bar: string;
+    iconBg: string;
+    border: string;
+  }
+> = {
   blue: {
     bar: "bg-blue-500",
     iconBg: "bg-blue-500/20",
@@ -42,28 +56,35 @@ const colorMap: Record<CardColor, {
 };
 
 const recentActivities = [
-  { id: 0, text: "Dodat novi predavac", date: Date.now() - (13 * 2592000 * 1000) }
+  {
+    id: 0,
+    text: "Dodat novi predavac",
+    date: Date.now() - 13 * 2592000 * 1000,
+  },
 ];
 
 const Dashboard = () => {
   const now = Date.now();
-  const userObject = useUser();
-  const user = userObject.user;
+  const { user, isLoaded } = useUser();
   const date = new Date();
   const currentHour = date.getHours();
-  const start = now - (60 * 60 * 1000);
+  const start = now - 60 * 60 * 1000;
   const end = now;
   
+  if (!isLoaded) {
+    return <DashboardSkeleton/>
+  }
+
   function generateGreeting() {
     if (currentHour < 12) {
       return "dobro jutro";
     } else if (currentHour >= 12 && currentHour < 18) {
       return "dobar dan";
     } else {
-      return "dobro veče"
+      return "dobro veče";
     }
-  };
-  
+  }
+
   function calculateTimeElapsed(start: number, end: number) {
     const elapsed = end - start;
     const elapsedInSeconds = elapsed / 1000;
@@ -86,13 +107,15 @@ const Dashboard = () => {
     } else {
       return Math.floor(elapsedInSeconds) + "s";
     }
-  };
+  }
   return (
-    <div className='w-full min-h-screen bg-linear-to-br from-white via-zinc-50 to-zinc-100 p-5 flex flex-col gap-10'>
+    <div className="w-full min-h-screen bg-linear-to-br from-white via-zinc-50 to-zinc-100 p-5 flex flex-col gap-10">
       <div className="w-full p-5 lg:p-7 rounded-xl border border-zinc-200/70 bg-white/40 backdrop-blur-lg shadow-[0_8px_30px_rgba(0,0,0,0.05)] flex flex-col gap-3 justify-start">
         <div>
           <p className="text-[0.8rem] uppercase">{generateGreeting()},</p>
-          <p className="text-2xl">{user?.firstName} {user?.lastName}</p>
+          <p className="text-2xl">
+            {user ? `${user.firstName} ${user.lastName}` : <DashboardSkeleton />}
+          </p>
         </div>
         <p className="text-sm">Dobrodošli u Atelier Studio admin panel.</p>
       </div>
@@ -101,7 +124,7 @@ const Dashboard = () => {
           const color = colorMap[card.color];
           return (
             <div key={index} className="w-full lg:w-1/4 relative bg-white/70 shadow-sm rounded-lg flex items-center gap-3 pt-4 pb-4 pl-3 pr-3 overflow-hidden">
-              <div className={`absolute left-0 top-0 h-full w-1 ${color.bar} rounded-l-xl`} />
+              <div className={`absolute left-0 top-0 h-full w-1 ${color.bar} rounded-l-xl`}/>
               <div className={`p-2 rounded-lg border ${color.iconBg} ${color.border} z-10 flex items-center justify-center`}>
                 {card.icon}
               </div>
@@ -116,24 +139,27 @@ const Dashboard = () => {
       <div className="w-full h-full flex flex-col mt-10 gap-10">
         <h1 className="text-3xl lg:text-4xl">Poslednje aktivnosti</h1>
         <ul>
-          {
-            recentActivities && recentActivities.length > 0 ? (
-              recentActivities.map((activity, index: number) => (
-                <li key={index} className="flex flex-row gap-20 items-center">
-                  <div className="flex flex-row gap-3 items-center">
-                    <div className="p-2 flex items-center justify-center rounded-lg bg-blue-500/20 border border-blue-500/25"><Clock size={20} /></div> {activity.text}
-                  </div>
-                  <span className="text-zinc-600 text-sm">{calculateTimeElapsed(activity.date, end)}</span>
-                </li>
-              ))
-            ) : (
-              <p>Ne postoje prethodne aktivnosti</p>
-            )
-          }
+          {recentActivities && recentActivities.length > 0 ? (
+            recentActivities.map((activity, index: number) => (
+              <li key={index} className="flex flex-row gap-20 items-center">
+                <div className="flex flex-row gap-3 items-center">
+                  <div className="p-2 flex items-center justify-center rounded-lg bg-blue-500/20 border border-blue-500/25">
+                    <Clock size={20} />
+                  </div>{" "}
+                  {activity.text}
+                </div>
+                <span className="text-zinc-600 text-sm">
+                  {calculateTimeElapsed(activity.date, end)}
+                </span>
+              </li>
+            ))
+          ) : (
+            <p>Ne postoje prethodne aktivnosti</p>
+          )}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
---
+++ components/DashboardSkeleton.tsx
@@ -0,0 +1,7 @@
+const DashboardSkeleton = () => {
+  return (
+    <p className="pb-3 text-white bg-gray-200/30 animate-pulse rounded-sm h-10 w-[60%]"></p>
+  )
+}
+
+export default DashboardSkeleton;
